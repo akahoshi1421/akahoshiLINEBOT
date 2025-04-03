6b549c7f9669e8ea9e425cbab9b98cf96ba37b3f
akahoshi1421 <akaho1421@gmail.com>
2025-04-03 23:44:26 +0900

    change: notify連携

--- src/app/utils/getOneNotifyScheduleMessage.ts
+++ src/app/utils/getOneNotifyScheduleMessage.ts
@@ -1,9 +1,12 @@
 import type { NotifySchedule } from "../types/lineMessageData";
+import { MessageCreate } from "./messageCreate";
 
 export const getOneNotifyScheduleMessage = (
   schedulesAll: NotifySchedule[],
   ago: "2週間前" | "1週間前" | "3日前" | "1日前"
 ) => {
+  const messageCreate = new MessageCreate();
+
   const schedulesMessages = schedulesAll.reduce((pre, schedule) => {
     const {
       イベント名: eventName,
@@ -14,19 +17,11 @@ export const getOneNotifyScheduleMessage = (
     return (
       pre +
       `
-  イベント名: ${eventName}
-  ${
-    eventDate
-      ? "集合時間: " +
-        Utilities.formatDate(eventDate, "JST", "yyyy-MM-dd HH:mm")
-      : ""
-  }
-  ${
-    schedule.participants.length
-      ? "参加者: " + schedule.participants.join(",")
-      : ""
-  }
-  ${remarks ? "備考: " + remarks : ""}
+${messageCreate.getEventNameMessage(eventName)}${messageCreate.getDateMessage(
+        eventDate
+      )}${messageCreate.getParticipantsMessage(
+        schedule.participants
+      )}${messageCreate.getRemakrsMessage(remarks)}
   `
     );
   }, `以下のスケジュールが${ago}です\n`);
